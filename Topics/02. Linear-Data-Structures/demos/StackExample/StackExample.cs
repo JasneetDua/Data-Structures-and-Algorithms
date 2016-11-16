b1c613b9ae1745dec1291ce037e6142777a31d4f
EvlogiHr <evlogihristov@yahoo.com>
2016-11-16 10:55:46 +0200

    Changed names in demo

--- Topics/02. Linear-Data-Structures/demos/StackExample/StackExample.cs
+++ Topics/02. Linear-Data-Structures/demos/StackExample/StackExample.cs
@@ -9,11 +9,14 @@ namespace StackExample
         {
             var stack = new Stack<string>();
 
-            stack.Push("1. Nikolay");
-            stack.Push("2. Ivo");
+            stack.Push("1. Viktor");
+            stack.Push("2. Marto");
             stack.Push("3. Doncho");
             stack.Push("4. Cuki");
             stack.Push("5. Evlogi");
+            stack.Push("6. Konstantin");
+            stack.Push("7. Ivan");
+            stack.Push("8. Steven");
 
             Console.WriteLine("Top = {0}", stack.Peek());
             while (stack.Count > 0)
