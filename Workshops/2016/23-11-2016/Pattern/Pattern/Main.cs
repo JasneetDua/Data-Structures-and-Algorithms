4837e796381626bd00707d6e6dd92ced0b8659cb
Kristiyan Tsaklev <cuki@cukii.tk>
2016-11-23 13:18:45 +0200

    Add tasks for workshop

---
+++ Workshops/2016/23-11-2016/Pattern/Pattern/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pattern
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+			// Your solution goes here
+			string figure = "urd";
+			Console.WriteLine(figure);
+
+			// comment before submitting
+			Svg.WriteToFile("output.svg", figure);
+		}
+	}
+}
