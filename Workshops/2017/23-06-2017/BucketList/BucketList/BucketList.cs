cbde8a1fe8a5d138d9e7d601eb1f71ab3c4f4a86
Kristiyan Tsaklev <cuki@cukii.tk>
2017-06-23 11:02:51 +0300

    Add workshop files

---
+++ Workshops/2017/23-06-2017/BucketList/BucketList/BucketList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BucketList
+{
+	public class BucketList<T> : IBucketList<T>
+	{
+		public T this[int index]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Size
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public void Add(T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(int index, T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
